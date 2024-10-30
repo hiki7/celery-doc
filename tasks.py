0a777b6f296bb3b383f399ede7d065f85d0a5ce6
hiki7 <rsaliyev03@gmail.com>
2024-10-30 18:45:01 +0500

    add: first task with notes

---
+++ tasks.py
@@ -0,0 +1,11 @@
+from celery import Celery
+
+#the first argument is the name of the module/file, it's needed so that names can be automatically generated
+#when the tasks are define in __main__
+
+#the second argument is the broker keyword argument, specifying the url of the message broker
+app = Celery('tasks', broker='pyamqp://guest@localhost//')
+
+@app.task
+def add(x, y):
+    return x + y
